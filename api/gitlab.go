a99a22703ddaf1f0388ed131859a3bffd1df00bb
zhaochanghao <zhaochanghao@xd.com>
2022-10-29 20:51:03 +0800

    搞定token

--- api/gitlab.go
+++ api/gitlab.go
@@ -1,3 +1,52 @@
 package api
 
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"net/url"
+)
+
+type GitlabToken struct {
+	AccessToken string `json:"access_token"`
+}
+
 // 代理 gitlab 接口
+
+func Login() http.Handler {
+	v := url.Values{}
+	v.Add("client_id", "712f0fa4c635f752f4efb99e7c0a8f90f6db97f8bb56f0f7fef3325e4b7659c5")
+	v.Add("redirect_uri", "http://localhost:4000/gitlab-callback")
+	v.Add("response_type", "code")
+	v.Add("state", "12345")
+	v.Add("scope", "api")
+	return http.RedirectHandler("https://gitlab.com/oauth/authorize?"+v.Encode(), 302)
+}
+
+func Callback(w http.ResponseWriter, r *http.Request) {
+
+	code := r.URL.Query().Get("code")
+	v := url.Values{}
+	v.Add("client_id", "712f0fa4c635f752f4efb99e7c0a8f90f6db97f8bb56f0f7fef3325e4b7659c5")
+	v.Add("client_secret", "3656bf06ebd645b21ca7088e99bfb40aa583ba5d4006c905a23b492ebc2b0896")
+	v.Add("redirect_uri", "http://localhost:4000/gitlab-callback")
+	v.Add("code", code)
+	v.Add("grant_type", "authorization_code")
+
+	resp, _ := http.PostForm("https://gitlab.com/oauth/token", v)
+	body, _ := io.ReadAll(resp.Body)
+
+	gitlabToken := &GitlabToken{}
+	err := json.Unmarshal([]byte(body), gitlabToken)
+	if err != nil {
+		fmt.Printf("序列化错误%v\n", err)
+	}
+	fmt.Println(gitlabToken)
+
+	c1 := http.Cookie{Name: "gitlab-token", Value: gitlabToken.AccessToken, HttpOnly: true, MaxAge: 7200, Path: "/", SameSite: 4, Secure: true}
+	w.Header().Add("Set-Cookie", c1.String())
+
+	http.Redirect(w, r, "/", 302)
+
+}
--- main.go
+++ main.go
@@ -1,14 +1,28 @@
 package main
 
 import (
-	"fmt"
-	"github.com/yisar/wcd/api"
-	"log"
 	"net/http"
-	"time"
+
+	"github.com/yisar/wcd/api"
 )
 
+func middleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// fmt.Println("前置操作")
+		next.ServeHTTP(w, r)
+		// fmt.Println("后置操作")
+	})
+}
+
+func root(w http.ResponseWriter, r *http.Request) {
+	// w.Header().Set("Content-Type", "application/json")
+	w.Write([]byte(`hello`))
+}
+
 func main() {
+
+	http.Handle("/", middleware(http.HandlerFunc(root)))
+
 	b := api.Broker{
 		Clients:        make(map[chan string]bool),
 		NewClients:     make(chan (chan string)),
@@ -19,16 +33,18 @@ func main() {
 	b.Start()
 
 	http.Handle("/events/", &b)
-	go func() {
-		for i := 0; ; i++ {
+	http.Handle("/login", api.Login())
+	http.Handle("/gitlab-callback", http.HandlerFunc(api.Callback))
 
-			b.Messages <- fmt.Sprintf("%d - the time is %v", i, time.Now())
+	// go func() {
+	// 	for i := 0; ; i++ {
 
-			log.Printf("Sent message %d ", i)
-			time.Sleep(1e9)
+	// 		b.Messages <- fmt.Sprintf("%d - the time is %v", i, time.Now())
 
-		}
-	}()
+	// 		log.Printf("Sent message %d ", i)
+	// 		time.Sleep(2e9)
 
-	http.ListenAndServe(":8000", nil)
+	// 	}
+	// }()
+	http.ListenAndServe(":4000", nil)
 }
